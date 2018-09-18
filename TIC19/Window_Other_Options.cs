8ded96c92ef2e30b28ce6ea824ccc4f844eb8a49
asandru <artister.hd@gmail.com>
2018-09-19 00:00:49 +0200

    Forgot somethingh
    Signed-off-by: asandru <artister.hd@gmail.com>

--- TIC19/Window_Other_Options.cs
+++ TIC19/Window_Other_Options.cs
@@ -19,17 +19,28 @@ namespace TIC19
 
         private void Watermark_myTextBox_Leave(object sender, EventArgs e)
         {
-
+            MyTextBox mTextBox = (MyTextBox)sender;
+            if (mTextBox.Text.Length == 0)
+            {
+                mTextBox.Text = "Required!";
+                mTextBox.ForeColor = Color.Red;
+            }
         }
 
         private void Watermark_myTextBox_Enter(object sender, EventArgs e)
         {
-
+            MyTextBox mTextBox = (MyTextBox)sender;
+            if (mTextBox.Text == "Required!")
+            {
+                mTextBox.Text = "";
+                mTextBox.ForeColor = Color.DimGray;
+            }
         }
 
         private void Watermark_myTextBox_KeyPress(object sender, KeyPressEventArgs e)
         {
-
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+                e.Handled = true;
         }
 
         private void Button1_Click(object sender, EventArgs e)
